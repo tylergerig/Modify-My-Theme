ace248610251adb9d92f57a944f65757ab008f37
Tyler Gerig <yvon328@hotmail.com>
2013-08-04 13:16:09 -0700

    Got title to work

--- header.php
+++ header.php
@@ -22,13 +22,13 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title>
-	<?php wp_title( '|', true, 'right' ); ?>
 	<?php
-		function assignPageTitle(){
+		function assignPageTitle($title){
 			return "I Am Awesome";
 		}
 		add_filter( 'wp_title', 'assignPageTitle');
 	?>
+	<?php wp_title( '|', true, 'right' ); ?>
 </title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
